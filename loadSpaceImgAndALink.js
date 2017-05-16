28895cd47745a184f5019fc283fb539fa62596d8
yinsang <1175180338@qq.com>
2017-05-16 18:28:03 +0800

    change loadSpace.js

--- loadSpaceImgAndALink.js
+++ loadSpaceImgAndALink.js
@@ -11,15 +11,18 @@ const nodeListToArray = (nodeList)=>{
 nodeListToArray(aLink).forEach(function(e){
 	if(e.getAttribute("href").slice(0,2) == "ht" || e.getAttribute("href").slice(0,2) == "//"){
 		e.setAttribute("href", "#")
-	}		
+	}
 
 
 })
 nodeListToArray(aImg).forEach(function(e){
 	if(e.getAttribute("src").slice(0,2) == "ht" || e.getAttribute("src").slice(0,2) == "//"){
+        if(e.indexOf("dont-change")  == -1){
+
 		e.setAttribute("src", "images/")
+        }
 	}
-	
+
 
 })
 alert("a链接和img已被初始化")
